0122eba508f273ce4b88256dc4239610da3b2157
SmitMavani <smitmavani926@gmail.com>
2026-09-18 21:51:38 +0530

    Fix Bearer token authorization

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,13 +1,17 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const token = req.header("Authorization");
+  const authHeader = req.header("Authorization");
 
-  if (!token) {
+  if (!authHeader) {
     return res.status(401).json({
       message: "No token, authorization denied",
     });
   }
+
+  const token = authHeader.startsWith("Bearer ")
+    ? authHeader.split(" ")[1]
+    : authHeader;
 
   try {
     const decoded = jwt.verify(token, "secretkey");
@@ -15,7 +19,6 @@ module.exports = (req, res, next) => {
     req.user = decoded;
 
     next();
-
   } catch (error) {
     res.status(401).json({
       message: "Token is not valid",
